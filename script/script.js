22143b1ce8bb5ae57bc8fe7963e8214a26791008
RayanAlkhelaiwi <r.n.m.k@live.com>
2019-11-13 14:22:03 +0300

    Added the add items function + enhancement in html and css
    For HTML: Added a container div. For CSS: Added background and text colors

--- script/script.js
+++ script/script.js
@@ -3,3 +3,24 @@
     For General Assembly's Front-End Web Development
     To-do List Website
 */
+
+$(document).ready(function () {
+
+    console.log("jQuery's Connected.");
+
+    var userInputBox = document.getElementById("userInputBox"); //$("#userInputBox");
+    var addBtn = $("#addBtn");
+    var itemsList = document.querySelector("ul"); // $("#items");
+
+    addBtn.click(function () {
+        if (userInputBox.value.length > 0) { // To make sure not to add empty items (without the user's input).
+            var createItems = document.createElement("li");
+            var itemsNode = document.createTextNode(userInputBox.value);
+            createItems.appendChild(itemsNode);
+            itemsList.appendChild(createItems);
+
+            userInputBox.value = ""; // Reset the Input box after adding the item.
+        }
+    });
+
+});

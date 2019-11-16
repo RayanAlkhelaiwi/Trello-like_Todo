e2ee820c1f4b75d5b1e053d19eb1269965c0467f
RayanAlkhelaiwi <r.n.m.k@live.com>
2019-11-17 00:27:14 +0300

    Enhanced CSS
    Along with additional enhancements in HTML (Adding extra classes as a result) and JS

--- script/script.js
+++ script/script.js
@@ -12,15 +12,45 @@ $(document).ready(function () {
     var addBtn = $("#addBtn");
     var itemsList = document.querySelector("ul"); // $("#items");
 
-    addBtn.click(function () {
+    // Accept items on button click
+    function addItemsClick() {
+
+        var createItems = document.createElement("li");
+        var itemsNode = document.createTextNode(userInputBox.value);
+
         if (userInputBox.value.length > 0) { // To make sure not to add empty items (without the user's input).
-            var createItems = document.createElement("li");
-            var itemsNode = document.createTextNode(userInputBox.value);
             createItems.appendChild(itemsNode);
             itemsList.appendChild(createItems);
-
-            userInputBox.value = ""; // Reset the Input box after adding the item.
         }
-    });
 
+        userInputBox.value = ""; // Reset the Input box after adding the item.
+
+        function completeItem() {
+            createItems.classList.toggle("completeItem");
+        }
+        createItems.addEventListener("click", completeItem);
+
+        // function deleteItem() {
+        //     var key = (event.keyCode ? event.keyCode : event.which); // Backspace button code = 8
+        //     if (key == 8) {
+        //         createItems.classList.add("deleteItem");
+        //     }
+        // }
+        // createItems.addEventListener("keypress", deleteItem);
+
+    }
+
+    // Accept items on Enter press (From keyboard)
+    function addItemsEnterPress() {
+        var key = (event.keyCode ? event.keyCode : event.which); // Enter button code = 13
+        if (key == 13) {
+            addItemsClick();
+        }
+    }
+
+    addBtn.click(addItemsClick);
+    userInputBox.addEventListener('keypress', addItemsEnterPress);
+
+    // var body = document.getElementsByTagName("body");
+    // body[0].innerHTML(); // Saves the state of the website in case of refresh (Saves entered items)
 });
